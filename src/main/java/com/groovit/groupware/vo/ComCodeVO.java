ec5030a0b40f011b8dd440d892f0e5f057bc542b
unknown <alswlsdl0912@naver.com>
2024-07-17 11:53:11 +0900

    first commit

---
+++ src/main/java/com/groovit/groupware/vo/ComCodeVO.java
@@ -0,0 +1,12 @@
+package com.groovit.groupware.vo;
+
+
+import lombok.Data;
+
+@Data
+public class ComCodeVO {
+	private String comCode;
+	private String comCodeNm;
+	private String comCodeUseYn;
+	
+}
